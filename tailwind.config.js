b162c0d2e89fcfc24fac76911f26767351f1ce9f
AmirMahdyJebreily <amirmahdyjdx@gmail.com>
2022-03-10 17:46:57 +0330

    initial commit
    add project files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            divideWidth: {
+                '1': '1px',
+            },
+        },
+    },
+    plugins: [],
+}
